380ae9f8d56c3815ea65a174780271de52f87f02
Rama Krishna Chunduri <ramakrishnachunduri@gmail.com>
2021-09-05 22:41:00 +0530

    Rename File
    - ProdItem rename to ProductItem for consistency

--- src/UI/ProductListUI.js
+++ src/UI/ProductListUI.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import CartableProducts from '../CartableProducts';
-import ProdItem from '../Components/ProdItem';
+import ProductItem from '../Components/ProductItem';
 
 export const ProductListUI = () => {
     return (
         <ul className="px-2 my-2 divide-y divide-gray-200">
         {CartableProducts.map(product => (
-                    <ProdItem key={product.id} productToRender={product} />
+                    <ProductItem key={product.id} productToRender={product} />
            ))}
         </ul>
     )
